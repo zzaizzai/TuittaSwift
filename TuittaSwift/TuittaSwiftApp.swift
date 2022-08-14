4106c4ad6730b0d887e0c81c7ed0f34bc55b7c34
junsai <junsai0530@gmail.com>
2022-08-14 21:37:59 +0900

    Initial Commit

---
+++ TuittaSwift/TuittaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuittaSwiftApp.swift
+//  TuittaSwift
+//
+//  Created by 小暮準才 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct TuittaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
